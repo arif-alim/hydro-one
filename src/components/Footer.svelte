d47502a95ebc55a7dc4398e7432dea06e1830577
Arif Alim <49820680+arif-alim@users.noreply.github.com>
2022-05-23 23:41:01 -0400

    removed readme

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,24 +1,3 @@
-<!-- <script>
-    import { path } from 'svelte-pathfinder'
-    import Link from '../components/Link.svelte'
-    import { appName } from '../lib/constants'
-</script>
-
-<footer class="flex flex-col items-center justify-center w-full h-24 font-thin">
-    <a
-        href="https://github.com/Greenheart/svelte-encrypted-spa"
-        class="pb-2"
-        target="_blank"
-        rel="noopener noreferrer">Created with {appName}</a
-    >
-
-    {#if $path.toString() !== '/'}
-        <Link href="/">Home</Link>
-    {/if}
-</footer> -->
-
-<!-- This example requires Tailwind CSS v2.0+ -->
-<!-- Footer Section: With Links Info Newsletter -->
 <footer id="page-footer" class="bg-white ">
     <div>
         <div
